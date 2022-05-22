0171dcfc9fb617ce33777fad19870f000172ead6
Shivaji Chalise <respond2shivaji@gmail.com>
2022-05-22 12:47:17 +0545

    Adding Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Navbar.css'
+
+import {HiMenuAlt3} from 'react-icons/hi'
+import {MdClose} from 'react-icons/md'
 
 const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false)
+
   return (
-    <div>
-      <p>Navbar</p>
-    </div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <a href="#home">shivajichalise.</a>
+      </div>
+
+      <ul className="app__navbar-links">
+        <li><a href="#about">about</a></li>
+        <li><a href="#works">works</a></li>
+        <li><a href="#contacts">contacts</a></li>
+      </ul>
+
+      <div className="app__navbar-sm">
+        <HiMenuAlt3 color="#5D9D0B" fontSize={27} onClick={() => {setToggleMenu(true)}} />
+
+        {toggleMenu && (
+          <div className="app__navbar-sm_overlay flex__center slide-bottom">
+            <MdClose fontSize={27} className="overlay__close" onClick={() => {setToggleMenu(false)}} />
+            <ul className="app__navbar-sm_links">
+              <li><a href="#about">about</a></li>
+              <li><a href="#works">works</a></li>
+              <li><a href="#contacts">contacts</a></li>
+            </ul>
+          </div>
+        )}
+
+      </div>
+    </nav>
   )
 }
 
