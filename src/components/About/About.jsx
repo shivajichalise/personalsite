7dff8506cfc25083e6f5c7c176f427f796a33605
Shivaji Chalise <respond2shivaji@gmail.com>
2022-05-22 17:48:03 +0545

    Adding About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,119 @@
 import React from 'react'
 import './About.css'
+import myPicture from '../../assets/portrait.png'
+import {HiCheckCircle} from 'react-icons/hi'
 
 const About = () => {
   return (
     <section id='about'>
-      About
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={myPicture} alt="Shivaji Chalise" />
+          </div>
+        </div>
+        <div className="about__content">
+          <p>
+            Hi! I'm Shivaji Chalise.
+            I'm a Software Engineering student
+            from Nepal on a mission to be a
+            strong problem solver. I'm motivated
+            to learn and expand my skill set
+            through challenging projects.
+            I love watching movies / TV shows,
+            play a little bit of guitar and.....
+            that's about it.
+          </p>
+        </div>
+      </div>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <HiCheckCircle className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <HiCheckCircle className='experience__details-icon' />
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <HiCheckCircle className='experience__details-icon' />
+              <div>
+                <h4>Javascript</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <HiCheckCircle className='experience__details-icon' />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <HiCheckCircle className='experience__details-icon' />
+              <div>
+                <h4>ReactJS</h4>
+                <small className='text-light'>Begineer</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <HiCheckCircle className='experience__details-icon' />
+              <div>
+                <h4>PHP</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <HiCheckCircle className='experience__details-icon' />
+              <div>
+                <h4>MySQLi</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <HiCheckCircle className='experience__details-icon' />
+              <div>
+                <h4>NodeJS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <HiCheckCircle className='experience__details-icon' />
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <HiCheckCircle className='experience__details-icon' />
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Begineer</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+      </div>
     </section>
   )
 }
