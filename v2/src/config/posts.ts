62fc7228a34e2136c5a7846c80d8f76e7f679fec
Shivaji Chalise <respond2shivaji@gmail.com>
2023-11-08 08:49:48 +0545

    added code to dynamically load the posts from a json file

--- v2/src/config/posts.ts
+++ v2/src/config/posts.ts
@@ -6,29 +6,7 @@ const posts: ContentProps[] = [
     description: "",
     points: {
       type: "decimal",
-      data: [
-        {
-          type: "link",
-          link: "/blog/1",
-          openInNewTab: false,
-          title: "How do show error bag in laravel?",
-          suffix: "Oct 24, 2023 ⋅ Code",
-        },
-        {
-          type: "link",
-          link: "/blog/2",
-          openInNewTab: false,
-          title: "How do show error bag in laravel?",
-          suffix: "Oct 26, 2023 ⋅ Code",
-        },
-        {
-          type: "link",
-          link: "/blog/3",
-          openInNewTab: false,
-          title: "Blog post 3",
-          suffix: "Oct 30, 2023 ⋅ Life",
-        },
-      ],
+      data: [],
     },
   },
 ];
